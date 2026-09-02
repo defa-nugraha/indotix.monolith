fed01706e6860b84069bea0474312245672b391a
defa-nugraha <starduststr0@gmail.com>
2026-09-02 22:38:50 +0700

    fix(ui): support required labels

--- resources/js/components/ui/label.tsx
+++ resources/js/components/ui/label.tsx
@@ -3,10 +3,16 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
+type LabelProps = React.ComponentProps<typeof LabelPrimitive.Root> & {
+  required?: boolean
+}
+
 function Label({
   className,
+  children,
+  required = false,
   ...props
-}: React.ComponentProps<typeof LabelPrimitive.Root>) {
+}: LabelProps) {
   return (
     <LabelPrimitive.Root
       data-slot="label"
@@ -15,7 +21,14 @@ function Label({
         className
       )}
       {...props}
-    />
+    >
+      {children}
+      {required ? (
+        <span aria-hidden="true" className="ml-0.5 text-red-600">
+          *
+        </span>
+      ) : null}
+    </LabelPrimitive.Root>
   )
 }
 
